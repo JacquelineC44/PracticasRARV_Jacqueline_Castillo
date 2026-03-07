ea9b7f2cdf7816d6baa431ae075b40e2049d810d
JacquelineC44 <jacque.cas4@gmail.com>
2026-03-06 23:20:46 -0600

    Practica2_Jacqueline_Castillo

--- Assets/ChangeColor.cs
+++ Assets/ChangeColor.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ChangeColor : MonoBehaviour
 {
-    public GameObject model;
+    public GameObject model;    
     public Color color;
     public Material colorMaterial;
     private Color colorOriginal;
